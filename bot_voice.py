cb53662bfaff95b74e717767ef80ca5f4c7e071e
roguemonk7 <132005034+roguemonk7@users.noreply.github.com>
2025-04-30 10:53:11 +0530

    Update bot_voice.py

--- bot_voice.py
+++ bot_voice.py
@@ -1,8 +1,8 @@
 import requests
 import os
 
-ELEVENLABS_API_KEY = "sk_22ae3ab298c013bb1d7aa349849f178595651c2e3cd97444"  # Ensure your API key is correct
-VOICE_ID = "ALgMVHkymAnEbk2koqfD"  # Replace with the ID of the voice you want to use
+ELEVENLABS_API_KEY = "sk_"  # Ensure your API key is correct
+VOICE_ID = "ALg"  # Replace with the ID of the voice you want to use
 
 # Function to convert text to speech using ElevenLabs
 def text_to_speech_with_elevenlabs(input_text, output_filepath):
